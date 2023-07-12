abe7f03f0ec3b76312d430d4f823950254fdaf79
tusharsurani <137286868+tusharsurani@users.noreply.github.com>
2023-07-12 16:42:59 +0530

    Add files via upload

---
+++ circlet20.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+main()
+{
+	int r,c,a=4,b=4;
+	for(r=1;r<=4;r++)
+	{
+		for(c=1;c<=7;c++)
+		{
+			if(c==a || c==b)
+			{
+				printf("*");
+			}
+			else
+			{
+				printf(" ");
+		    }
+		}
+		a++;
+	    b--;
+	    printf("\n");
+	}
+}
